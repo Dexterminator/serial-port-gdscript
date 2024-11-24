f5b94301554f30e14cc9ab21b19f994b51457a76
Dexter Gramfors <dexter.gramfors@gmail.com>
2024-11-24 11:54:35 -0500

    Make SerialPortHandler autoload

--- test_serial_port.gd
+++ test_serial_port.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
-@onready var serial_port_handler = $SerialPortHandler
-
 
 func _ready():
-	var data = serial_port_handler.read()
+	var data = SerialPortHandler.read()
 	if data != "":
 		print("Received Data: ", data)
 
-	serial_port_handler.write("in")
+	SerialPortHandler.write("in")
